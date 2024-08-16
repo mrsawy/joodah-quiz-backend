3fb2c55f02b12627edbfb0e3f6c9750f5d17f9bf
mrsawy <mrsawy1@gmail.com>
2024-08-16 06:16:59 +0300

    feat: rebuild front-build folder with the sort feature

---
+++ static/js/754.6716f4c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkquiz_app=self.webpackChunkquiz_app||[]).push([[754],{6754:(s,e,u)=>{u.r(e),u.d(e,{default:()=>r});var a=u(7578),c=u(6113),i=u(7010),o=u(3612),t=u(2980),n=u(2882);const r=function(s){let{children:e}=s;const u=(0,c.wA)(),{checkIsDone:r,isError:d,isLoading:l,isSuccess:h,isLogged:f}=(0,c.d4)((s=>s.auth));return(0,a.useLayoutEffect)((()=>{u((0,t.F2)())}),[]),l||!r?(0,n.jsx)(o.A,{className:"h-full"}):d||!f?(0,n.jsx)(i.C5,{to:"/dashboard/login"}):h&&f?e:void 0}}}]);
+//# sourceMappingURL=754.6716f4c3.chunk.js.map
