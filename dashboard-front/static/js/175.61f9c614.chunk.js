e59eb0f4fa9cefd08d0b365633708c3065452d2b
ahmed mrsawy <mrsawy1@gmail.com>
2025-06-06 16:37:17 +0300

    added dashboard , frontend builds  after edits

---
+++ dashboard-front/static/js/175.61f9c614.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkquiz_app=self.webpackChunkquiz_app||[]).push([[175],{175:(s,e,u)=>{u.r(e),u.d(e,{default:()=>r});var a=u(5825),c=u(8712),i=u(6537),o=u(7782),t=u(5982),n=u(2485);const r=function(s){let{children:e}=s;const u=(0,c.wA)(),{checkIsDone:r,isError:d,isLoading:l,isSuccess:h,isLogged:f}=(0,c.d4)((s=>s.auth));return(0,a.useLayoutEffect)((()=>{u((0,t.F2)())}),[]),l||!r?(0,n.jsx)(o.A,{className:"h-full"}):d||!f?(0,n.jsx)(i.C5,{to:"/dashboard/login"}):h&&f?e:void 0}}}]);
+//# sourceMappingURL=175.61f9c614.chunk.js.map
