4f7174fb083ae2db3ee298eff259d5f69338ac6d
ahmed mrsawy <mrsawy1@gmail.com>
2025-06-21 16:42:51 +0300

    relocated the config.js file to the static folder

--- static/config.js
+++ static/config.js
@@ -3,8 +3,10 @@
 //     UPLOADS_URL: "http://localhost:4000/uploads/",
 //     API_URL: "http://localhost:4000/api",   
 // };
+
 window._env_ = {
     BASE_URL: "https://web.hadefah.com",
     UPLOADS_URL: "https://web.hadefah.com/uploads/",
     API_URL: "https://web.hadefah.com/api",   
 };
+
