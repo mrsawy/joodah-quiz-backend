58014df047f87303486cfd81bc783dcebe4086a4
mrsawy <mrsawy1@gmail.com>
2025-06-26 16:06:27 +0300

    updating language errors

--- dashboard-front/config.js
+++ dashboard-front/config.js
@@ -3,3 +3,10 @@ window._env_ = {
     UPLOADS_URL: "http://localhost:4000/uploads/",
     API_URL: "http://localhost:4000/api",   
 };
+
+// window._env_ = {
+//     BASE_URL: "https://web.hadefah.com",
+//     UPLOADS_URL: "https://web.hadefah.com/uploads/",
+//     API_URL: "https://web.hadefah.com/api",   
+// };
+
